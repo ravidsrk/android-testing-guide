557ed6610c00f9a40883e8d5cf0649a98ed0d22f
Ravindra Kumar <ravidsrk@gmail.com>
2016-09-08 15:25:35 +0530

    Added tests with junit filters

--- SampleApp/app/src/androidTest/java/in/ravidsrk/sample/MainActivityTest.java
+++ SampleApp/app/src/androidTest/java/in/ravidsrk/sample/MainActivityTest.java
@@ -1,8 +1,15 @@
 package in.ravidsrk.sample;
 
 import android.app.Activity;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.filters.RequiresDevice;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.filters.SmallTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.junit.Rule;
@@ -20,6 +27,9 @@ public class MainActivityTest {
     @Rule
     public MainActivityTestRule<MainActivity> mainActivityActivityTestRule = new MainActivityTestRule<MainActivity>(MainActivity.class);
 
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
     @Test
     public void testUI() {
         Activity activity = mainActivityActivityTestRule.getActivity();
@@ -29,5 +39,45 @@ public class MainActivityTest {
         assertEquals("Hello World!", helloView.getText());
         assertEquals(InstrumentationRegistry.getTargetContext().getString(R.string.hello_world), helloView.getText());
         assertNull(activity.findViewById(android.R.id.button1));
+    }
+
+    @Test
+    @RequiresDevice
+    public void testRequiresDevice() {
+        Log.d("Test Filters", "This test requires a device");
+        Activity activity = activityTestRule.getActivity();
+        assertNotNull("MainActivity is not available", activity);
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = 30)
+    public void testMinSdkVersion() {
+        Log.d("Test Filters", "Checking for min sdk >= 30");
+        Activity activity = activityTestRule.getActivity();
+        assertNotNull("MainActivity is not available", activity);
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.LOLLIPOP)
+    public void testMinBuild() {
+        Log.d("Test Filters", "Checking for min build > Lollipop");
+        Activity activity = activityTestRule.getActivity();
+        assertNotNull("MainActivity is not available", activity);
+    }
+
+    @Test
+    @SmallTest
+    public void testSmallTest() {
+        Log.d("Test Filters", "this is a small test");
+        Activity activity = activityTestRule.getActivity();
+        assertNotNull("MainActivity is not available", activity);
+    }
+
+    @Test
+    @LargeTest
+    public void testLargeTest() {
+        Log.d("Test Filters", "This is a large test");
+        Activity activity = activityTestRule.getActivity();
+        assertNotNull("MainActivity is not available", activity);
     }
 }
